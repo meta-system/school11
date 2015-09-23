857c9d44ef0c081bc911ec7600030cd042b206bb
meta_system <meta_system@online.degit config --global user.email meta_system@online.degit config --global user.email meta_system@online.de>
2015-09-23 22:47:48 +0200

    "Converted to IDEA and finished refactoring"

--- src/sortingAlgorithms/SortAlgorithms.java
+++ src/sortingAlgorithms/SortAlgorithms.java
@@ -1,5 +1,92 @@
 package sortingAlgorithms;
 
 public class SortAlgorithms {
+	
+	public static int[] mSort(int[] array){
+		int N = array.length;
+
+
+		if (N<=1){
+			return array;
+		}
+		int middle = N/2;
+		int [] lArray = new int [middle];
+		int [] rArray = new int [N-middle];
+
+		System.arraycopy(array, 0, lArray, 0, middle);
+		System.arraycopy(array, middle, rArray, 0, (N-middle));
+
+		lArray = mSort(lArray);
+		rArray = mSort(rArray);
+		array = merge(array, lArray, rArray);
+		return array;
+	}
+
+	private static int[] merge(int[] array, int[] lArray, int[] rArray) {
+		int lIndex = 0;
+		int rIndex = 0;
+		for (int i = 0; i < array.length; i++){
+			if((lIndex < lArray.length)&&(rIndex < rArray.length)){
+				if (lArray[lIndex] < rArray[rIndex]){
+					array[i] = lArray[lIndex];
+					lIndex++;
+				}
+				else {
+					array[i] = rArray[rIndex];
+					rIndex++;
+				}
+			}
+			else {
+				if (lIndex < lArray.length){
+					array[i] = lArray[lIndex];
+					lIndex++;
+				}
+				else {
+					array[i] = rArray[rIndex];
+					rIndex++;
+
+				}	
+			}	
+		}
+		return array;
+	}
+
+	public static int[] bSort(int [] array) {
+		int temp;
+		boolean swap;
+		do {
+			swap = false;
+			for (int i = 0; i < (array.length-1); i++) {
+				if (array[i] > array[i + 1]) {
+					temp = array[i + 1];
+					array[i + 1] = array[i];
+					array[i] = temp;
+					swap = true;
+				}
+			}
+		} while (swap);
+		
+		return array;
+	}
+
+	public static int[] iSort(int [] array){
+		int temp;
+		for (int i = 0; i < array.length; i++){
+			temp = array[i];
+			for (int j = (i-1); 0 <= j; j--){
+				if (array[j] > temp){
+					array[j+1] = array[j];
+					if (j == 0){
+						array[j]=temp;
+					}
+				} else {
+					array[j+1] = temp;
+					//j = 0;
+					break;
+				}
+			}
+		}
+		return array;
+	}
 
 }
--- src/sortingAlgorithms/SortMain.java
+++ src/sortingAlgorithms/SortMain.java
@@ -1,5 +1,49 @@
 package sortingAlgorithms;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class SortMain {
+
+    public static int[] toBeSorted = new int[20];
+    public static int[] sorted = new int[toBeSorted.length];
+
+    public static void main(String[] args) {
+        genNumbs();	//das füllt den Array toBeSorted mit zufälligen Zahlen aus
+        Scanner sc = new Scanner(System.in);
+       // SortAlgorithms sAlgs = new SortAlgorithms();
+        boolean test = true;
+
+        System.out.println("unsortiert: " + Arrays.toString(toBeSorted));
+
+        do {
+            System.out.println("Bitte geben Sie ein, welchen Sortieralgorithmus Sie verwenden wollen:"
+                    + "\n [1] Bubblesort"
+                    + "\n [2] Insertsort"
+                    + "\n [3] Mergesort");
+
+            switch (sc.nextInt()){
+                case 1: sorted = SortAlgorithms.bSort(toBeSorted); break;
+                case 2: sorted = SortAlgorithms.iSort(toBeSorted); break;
+                case 3: sorted = SortAlgorithms.mSort(toBeSorted); break;
+                default: System.out.println("Fehlerhafte Eingabe!");
+                    test = false;
+                    break;
+            }
+        } while (!test);
+
+        System.out.println("\nsortiert:   " + Arrays.toString(sorted));
+        sc.close();
+
+    }
+
+    public static void genNumbs(){
+        Random rand = new Random();
+        for(int i = 0; i < toBeSorted.length; i++){
+            toBeSorted[i] = rand.nextInt(20) + 1;
+        }
+
+    }
 
 }
